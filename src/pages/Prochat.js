b68d78f57aa97d5d32efb7df787c35540b443f80
2chaos <1585767724@qq.com>
2024-03-14 23:23:35 +0800

    将prochat中的request替换为实际内容

--- src/pages/Prochat.js
+++ src/pages/Prochat.js
@@ -4,13 +4,17 @@ import { Button } from "antd";
 import { useTheme } from "antd-style";
 import { LeftOutlined } from "@ant-design/icons";
 import React from "react";
+
 export default function Prochat({ setClicked }) {
   const theme = useTheme();
   const [showComponent, setShowComponent] = useState(false);
+
   useEffect(() => setShowComponent(true), []);
+
   const handleReturn = () => {
     setClicked(false);
   };
+
   return (
     <div
       style={{
@@ -40,8 +44,77 @@ export default function Prochat({ setClicked }) {
             "欢迎使用 帮你读 ，我是你的专属机器人，你将要查询的科目是{。。。}，请输入需要查询的内容。"
           }
           request={async (messages) => {
-            const mockedData = `这是一段模拟的对话数据。本次会话传入了${messages.length}条消息`;
-            return new Response(mockedData);
+            try {
+              const embedding = await openai.embeddings.create({
+                model: "text-embedding-ada-002",
+                input: messages,
+                encoding_format: "float",
+              });
+              const embeddingData = embedding.data[0].embedding;
+
+              const client = new QdrantClient({
+                url: process.env.QDRANT_URL,
+                apiKey: process.env.QDRANT_APIKEY,
+              });
+              const collectionName = "test_collection";
+              const res1 = await client.search(collectionName, {
+                vector: embeddingData,
+                limit: 1,
+              });
+
+              const encoder = new TextEncoder();
+              const stream = new ReadableStream({
+                async start(controller) {
+                  try {
+                    const chatData = await openai.chat.completions.create({
+                      model: "gpt-3.5-turbo",
+                      messages: [
+                        {
+                          role: "user",
+                          content: `\n
+                            问题："""${messages}"""
+                            可能的答案:"""${JSON.stringify(content)}"""
+                            \n
+                            基于以上的问题和可能的答案总结一个得体并且言简意骇的回答，只需要输出回答即可。
+                            例如：
+                              > 在.umijs.js中无法使用require.context，因为.umijs.js不是在浏览器环境下运行，而是通过node的fs进行处理。
+                        `,
+                        },
+                      ],
+                      temperature: 1,
+                      frequency_penalty: 0,
+                      presence_penalty: 0,
+                      max_tokens: 2000,
+                      stream: true,
+                    });
+
+                    for await (const part of chatData) {
+                      controller.enqueue(
+                        encoder.encode(part.choices[0]?.delta?.content || "")
+                      );
+                    }
+                    // 完成后，关闭流
+                    controller.close();
+                  } catch (e) {
+                    // 如果在执行过程中发生错误，向流发送错误
+                    controller.error(e);
+                  }
+                },
+              });
+
+              return new Response(stream);
+            } catch (error) {
+              console.log(error);
+              const res = new Response(
+                JSON.stringify({
+                  message: "Internal server error" + error.message,
+                }),
+                {
+                  status: 500,
+                }
+              );
+              return res;
+            }
           }}
         />
       )}
--- src/pages/api/query.js
+++ src/pages/api/query.js
@@ -1,93 +1,124 @@
-import OpenAI from "openai";
-import { QdrantClient } from "@qdrant/js-client-rest";
+import { useState, useEffect } from "react";
+import { ProChat } from "@ant-design/pro-chat";
+import { Button } from "antd";
+import { useTheme } from "antd-style";
+import { LeftOutlined } from "@ant-design/icons";
+import React from "react";
+import { message } from "antd";
 
-const openai = new OpenAI({
-  apiKey: process.env.API_KEY,
-});
-export const config = {
-  runtime: "edge",
-};
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const { query, array } = await req.json();
+export default function Prochat({ setClicked }) {
+  const theme = useTheme();
+  const [showComponent, setShowComponent] = useState(false);
 
-      const embedding = await openai.embeddings.create({
-        model: "text-embedding-ada-002",
-        input: query,
-        encoding_format: "float",
-      });
-      const embeddingData = embedding.data[0].embedding;
+  useEffect(() => setShowComponent(true), []);
 
-      const client = new QdrantClient({
-        url: process.env.QDRANT_URL,
-        apiKey: process.env.QDRANT_APIKEY,
-      });
-      const collectionName = "test_collection";
-      const res1 = await client.search(collectionName, {
-        vector: embeddingData,
-        limit: 1,
-      });
-      let content = array[res1[0]?.id];
+  const handleReturn = () => {
+    setClicked(false);
+  };
 
-      const encoder = new TextEncoder();
-      const stream = new ReadableStream({
-        async start(controller) {
-          try {
-            const chatData = await openai.chat.completions.create({
-              model: "gpt-3.5-turbo",
-              messages: [
-                {
-                  role: "user",
-                  content: `\n
-      问题："""${query}"""
-      可能的答案:"""${JSON.stringify(content)}"""
-      \n
-      基于以上的问题和可能的答案总结一个得体并且言简意骇的回答，只需要输出回答即可。
-      例如：
-        > 在.umijs.js中无法使用require.context，因为.umijs.js不是在浏览器环境下运行，而是通过node的fs进行处理。
-                  `,
-                },
-              ],
-              temperature: 1,
-              frequency_penalty: 0,
-              presence_penalty: 0,
-              max_tokens: 2000,
-              stream: true,
-            });
+  return (
+    <div
+      style={{
+        backgroundColor: theme.colorBgLayout,
+      }}
+    >
+      <div style={{ position: "fixed", top: "8px", left: "8px", zIndex: 999 }}>
+        <Button
+          style={{
+            position: "absolute",
+            top: "8px",
+            left: "8px",
+          }}
+          onClick={handleReturn}
+        >
+          <LeftOutlined />
+        </Button>
+      </div>
 
-            for await (const part of chatData) {
-              controller.enqueue(
-                encoder.encode(part.choices[0]?.delta?.content || "")
-              );
-            }
-            // 完成后，关闭流
-            controller.close();
-          } catch (e) {
-            // 如果在执行过程中发生错误，向流发送错误
-            controller.error(e);
+      {showComponent && (
+        <ProChat
+          style={{
+            height: "100vh",
+            width: "100vw",
+          }}
+          helloMessage={
+            "欢迎使用 帮你读 ，我是你的专属机器人，你将要查询的科目是{。。。}，请输入需要查询的内容。"
           }
-        },
-      });
+          request={async (messages) => {
+            try {
+              const embedding = await openai.embeddings.create({
+                model: "text-embedding-ada-002",
+                input: messages,
+                encoding_format: "float",
+              });
+              const embeddingData = embedding.data[0].embedding;
 
-      return new Response(stream);
-    } catch (error) {
-      console.log(error);
-      const res = new Response(
-        JSON.stringify({
-          message: "Internal server error" + error.message,
-        }),
-        {
-          status: 500,
-        }
-      );
-      return res;
-    }
-  } else {
-    const res = new Response({
-      status: 405,
-      statusText: "Method not allowed",
-    });
-    return res;
-  }
+              const client = new QdrantClient({
+                url: process.env.QDRANT_URL,
+                apiKey: process.env.QDRANT_APIKEY,
+              });
+              const collectionName = "test_collection";
+              const res1 = await client.search(collectionName, {
+                vector: embeddingData,
+                limit: 1,
+              });
+
+              const encoder = new TextEncoder();
+              const stream = new ReadableStream({
+                async start(controller) {
+                  try {
+                    const chatData = await openai.chat.completions.create({
+                      model: "gpt-3.5-turbo",
+                      messages: [
+                        {
+                          role: "user",
+                          content: `\n
+                            问题："""${messages}"""
+                            可能的答案:"""${JSON.stringify(content)}"""
+                            \n
+                            基于以上的问题和可能的答案总结一个得体并且言简意骇的回答，只需要输出回答即可。
+                            例如：
+                              > 在.umijs.js中无法使用require.context，因为.umijs.js不是在浏览器环境下运行，而是通过node的fs进行处理。
+                        `,
+                        },
+                      ],
+                      temperature: 1,
+                      frequency_penalty: 0,
+                      presence_penalty: 0,
+                      max_tokens: 2000,
+                      stream: true,
+                    });
+
+                    for await (const part of chatData) {
+                      controller.enqueue(
+                        encoder.encode(part.choices[0]?.delta?.content || "")
+                      );
+                    }
+                    // 完成后，关闭流
+                    controller.close();
+                  } catch (e) {
+                    // 如果在执行过程中发生错误，向流发送错误
+                    controller.error(e);
+                  }
+                },
+              });
+
+              return new Response(stream);
+            } catch (error) {
+              console.log(error);
+              const res = new Response(
+                JSON.stringify({
+                  message: "Internal server error" + error.message,
+                }),
+                {
+                  status: 500,
+                }
+              );
+              return res;
+            }
+          }}
+        />
+      )}
+    </div>
+  );
 }
