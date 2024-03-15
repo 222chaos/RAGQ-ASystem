72885e6a5002590999c35c3a5b84113ceeafef30
2chaos <1585767724@qq.com>
2024-03-15 23:14:01 +0800

    添加调试api

--- src/pages/Prochat.js
+++ src/pages/Prochat.js
@@ -8,11 +8,32 @@ import React from "react";
 export default function Prochat({ setClicked }) {
   const theme = useTheme();
   const [showComponent, setShowComponent] = useState(false);
-
   useEffect(() => setShowComponent(true), []);
 
   const handleReturn = () => {
     setClicked(false);
+  };
+
+  const sendMessage = async (messages) => {
+    try {
+      const response = await fetch("/api/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to send message");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error sending message:", error);
+      return null;
+    }
   };
 
   return (
@@ -40,82 +61,8 @@ export default function Prochat({ setClicked }) {
             height: "100vh",
             width: "100vw",
           }}
-          helloMessage={
-            "欢迎使用 帮你读 ，我是你的专属机器人，你将要查询的科目是{。。。}，请输入需要查询的内容。"
-          }
-          request={async (messages) => {
-            try {
-              const embedding = await openai.embeddings.create({
-                model: "text-embedding-ada-002",
-                input: messages,
-                encoding_format: "float",
-              });
-              const embeddingData = embedding.data[0].embedding;
-
-              const client = new QdrantClient({
-                url: process.env.QDRANT_URL,
-                apiKey: process.env.QDRANT_APIKEY,
-              });
-              const collectionName = "test_collection";
-              const res1 = await client.search(collectionName, {
-                vector: embeddingData,
-                limit: 1,
-              });
-
-              const encoder = new TextEncoder();
-              const stream = new ReadableStream({
-                async start(controller) {
-                  try {
-                    const chatData = await openai.chat.completions.create({
-                      model: "gpt-3.5-turbo",
-                      messages: [
-                        {
-                          role: "user",
-                          content: `\n
-                            问题："""${messages}"""
-                            可能的答案:"""${JSON.stringify(content)}"""
-                            \n
-                            基于以上的问题和可能的答案总结一个得体并且言简意骇的回答，只需要输出回答即可。
-                            例如：
-                              > 在.umijs.js中无法使用require.context，因为.umijs.js不是在浏览器环境下运行，而是通过node的fs进行处理。
-                        `,
-                        },
-                      ],
-                      temperature: 1,
-                      frequency_penalty: 0,
-                      presence_penalty: 0,
-                      max_tokens: 2000,
-                      stream: true,
-                    });
-
-                    for await (const part of chatData) {
-                      controller.enqueue(
-                        encoder.encode(part.choices[0]?.delta?.content || "")
-                      );
-                    }
-                    // 完成后，关闭流
-                    controller.close();
-                  } catch (e) {
-                    // 如果在执行过程中发生错误，向流发送错误
-                    controller.error(e);
-                  }
-                },
-              });
-
-              return new Response(stream);
-            } catch (error) {
-              console.log(error);
-              const res = new Response(
-                JSON.stringify({
-                  message: "Internal server error" + error.message,
-                }),
-                {
-                  status: 500,
-                }
-              );
-              return res;
-            }
-          }}
+          helloMessage={"欢迎使用 帮你读 ，我是你的专属机器人，这"}
+          request={sendMessage}
         />
       )}
     </div>
--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,34 +1,24 @@
 import OpenAI from "openai";
-import { QdrantClient } from "@qdrant/js-client-rest";
+import { v4 as uuidv4 } from "uuid";
 
 const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
+
 export const config = {
   runtime: "edge",
 };
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      const { query, array } = await req.json();
-
-      const embedding = await openai.embeddings.create({
-        model: "text-embedding-ada-002",
-        input: query,
-        encoding_format: "float",
-      });
-      const embeddingData = embedding.data[0].embedding;
-
-      const client = new QdrantClient({
-        url: process.env.QDRANT_URL,
-        apiKey: process.env.QDRANT_APIKEY,
-      });
-      const collectionName = "test_collection";
-      const res1 = await client.search(collectionName, {
-        vector: embeddingData,
-        limit: 1,
-      });
-      let content = array[res1[0]?.id];
+      const { messages } = await req.json();
+      const chatMessages = messages.map((message) => ({
+        role: "user",
+        content: message.content,
+      }));
+      const userId = uuidv4();
+      const rolePlayText = ` `;
 
       const encoder = new TextEncoder();
       const stream = new ReadableStream({
@@ -37,22 +27,12 @@ export default async function handler(req, res) {
             const chatData = await openai.chat.completions.create({
               model: "gpt-3.5-turbo",
               messages: [
-                {
-                  role: "user",
-                  content: `\n
-      问题："""${query}"""
-      可能的答案:"""${JSON.stringify(content)}"""
-      \n
-      基于以上的问题和可能的答案总结一个得体并且言简意骇的回答，只需要输出回答即可。
-      例如：
-        > 在.umijs.js中无法使用require.context，因为.umijs.js不是在浏览器环境下运行，而是通过node的fs进行处理。
-                  `,
-                },
+                { role: "system", content: rolePlayText },
+                { role: "user", content: `UserId: ${userId}` },
+                ...chatMessages,
               ],
               temperature: 1,
-              frequency_penalty: 0,
-              presence_penalty: 0,
-              max_tokens: 2000,
+              max_tokens: 888,
               stream: true,
             });
 
@@ -72,7 +52,6 @@ export default async function handler(req, res) {
 
       return new Response(stream);
     } catch (error) {
-      console.log(error);
       const res = new Response(
         JSON.stringify({
           message: "Internal server error" + error.message,
