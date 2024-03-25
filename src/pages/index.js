e956f89932475c5731343694d86bc75e5cee244c
2chaos <1585767724@qq.com>
2024-03-25 23:41:48 +0800

    设置 API 路由responseLimit:false

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,205 +1,64 @@
 import React, { useState } from "react";
-import { Button, Input, Menu } from "antd";
-import {
-  UploadOutlined,
-  SearchOutlined,
-  NumberOutlined,
-} from "@ant-design/icons";
-
-const { TextArea } = Input;
-const utf8Decoder = new TextDecoder("utf-8");
+import { Button } from "antd";
+import CenterMode from "./Carousel";
+import Prochat from "./Prochat";
 
 const IndexPage = () => {
-  const [content, setContent] = useState("");
-  const [text, setText] = useState("");
-  const [query, setQuery] = useState("");
-  const [response, setResponse] = useState("...");
-  const [array, setArray] = useState([]);
-  const [uploading, setUploading] = useState(false);
-  const [selectedMenuItem, setSelectedMenuItem] = useState("upload");
-
+  const [clicked, setClicked] = useState(false);
+  const [selectedImageInfo, setSelectedImageInfo] = useState(null);
   const handleContentSubmit = async () => {
     try {
-      setUploading(true);
-      const res = await fetch(
-        /*http://127.0.0.1:5000/fenci*/ "/api/embedding",
-        {
-          method: "POST",
-          body: JSON.stringify({ content }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = await res.json();
-      setArray(data);
-
-      setResponse("上传已完成");
-
-      setTimeout(() => {
-        setResponse("");
-      }, 2000);
-    } catch (error) {
-      console.error("内容上传请求出错:", error);
-    } finally {
-      setUploading(false);
-    }
-  };
-
-  const handleQuerySubmit = async () => {
-    setResponse("");
-    let tempText = "";
-
-    const requestData = {
-      query: query,
-      array: array,
-    };
-    try {
-      const queryRes = await fetch("/api/query", {
+      const res = await fetch("/api/embedding", {
         method: "POST",
+        body: JSON.stringify({}),
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(requestData),
       });
-      const reader = queryRes.body.getReader();
-
-      const processData = ({ done, value: chunk }) => {
-        if (done) {
-          console.log("Stream finished");
-          return;
-        }
-        setResponse((response) => {
-          return response + utf8Decoder.decode(chunk, { stream: true });
-        });
-
-        tempText += utf8Decoder.decode(chunk, { stream: true });
-
-        return reader.read().then(processData);
-      };
-      await processData(await reader.read());
+      const data = await res.json();
+      console.log(data);
+      console.log("开始上传");
     } catch (error) {
-      console.log(error);
       console.error("内容上传请求出错:", error);
-    }
-  };
-
-  const menuItemClickHandler = async (item) => {
-    setSelectedMenuItem(item.key);
-
-    try {
-      let filePath = "";
-      if (item.key === "junior1") {
-        filePath = "/jsjwl.txt";
-      } else if (item.key === "junior2") {
-        filePath = "/xqgc.txt";
-      }
-
-      if (filePath) {
-        setUploading(true);
-        const response = await fetch(filePath);
-        const contentBuffer = await response.arrayBuffer();
-        const content = utf8Decoder.decode(contentBuffer);
-        setText(content);
-        const aiRes = await fetch("/api/ai", {
-          method: "POST",
-          body: JSON.stringify({ content: text }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const aiData = await aiRes.json();
-        setArray(aiData);
-      }
-    } catch (error) {
-      console.error("读取文件或调用api/ai出错:", error);
-    } finally {
-      setUploading(false);
     }
   };
 
   return (
     <div>
-      <Menu
-        mode="horizontal"
-        style={{ textAlign: "center" }}
-        selectedKeys={[selectedMenuItem]}
-        onClick={menuItemClickHandler}
-      >
-        <Menu.SubMenu key="freshman" icon={<SearchOutlined />} title="大一">
-          <Menu.Item key="freshiman1">课程1</Menu.Item>
-          <Menu.Item key="freshiman2">课程2</Menu.Item>
-        </Menu.SubMenu>
-        <Menu.SubMenu key="sophomore" icon={<SearchOutlined />} title="大二">
-          <Menu.Item key="sophomore1">课程1</Menu.Item>
-          <Menu.Item key="sophomore2">课程2</Menu.Item>
-        </Menu.SubMenu>
-        <Menu.SubMenu key="junior" icon={<SearchOutlined />} title="大三">
-          <Menu.Item key="junior1">计算机网络</Menu.Item>
-          <Menu.Item key="junior2">需求工程</Menu.Item>
-        </Menu.SubMenu>
-        <Menu.SubMenu key="senior" icon={<SearchOutlined />} title="大四">
-          <Menu.Item key="senior1">课程1</Menu.Item>
-          <Menu.Item key="senior2">课程2</Menu.Item>
-        </Menu.SubMenu>
-      </Menu>
-
-      <div style={{ marginLeft: "220px" }}>
-        <h1>、</h1>
-        <div style={{ margin: "auto", width: "50%", textAlign: "center" }}>
-          <TextArea
-            showCount
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            placeholder="请上传内容"
-            style={{ height: 120, resize: "none" }}
-          />
-          <br />
-          <br />
-          <Button
-            style={{ height: "50px", width: "300px" }}
-            onClick={handleContentSubmit}
-            type="primary"
-          >
-            上传
-          </Button>
+      {clicked ? (
+        <div></div>
+      ) : (
+        <div style={{ position: "relative" }}>
+          <h1 style={{ marginLeft: "20px", position: "absolute" }}>帮你读</h1>
+          <div style={{ position: "fixed", bottom: 20, right: 20 }}>
+            {/* 将按钮放置在页面右下角 */}
+            <Button type="primary" onClick={handleContentSubmit}>
+              提交
+            </Button>
+          </div>
         </div>
-        <br />
-        <br />
+      )}
+      {clicked ? (
+        <Prochat
+          setClicked={setClicked}
+          selectedImageInfo={selectedImageInfo}
+        />
+      ) : (
         <div
           style={{
-            margin: "auto",
-            width: "50%",
-            textAlign: "center",
-            marginTop: "20px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
           }}
         >
-          <Input
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="请输入查询内容"
-            style={{ width: "100%", height: "50px" }}
-            disabled={uploading}
+          <CenterMode
+            setClicked={setClicked}
+            setSelectedImageInfo={setSelectedImageInfo}
           />
-          <br />
-          <br />
-          <Button
-            style={{ height: "50px", width: "300px" }}
-            onClick={handleQuerySubmit}
-            type="primary"
-            disabled={uploading}
-          >
-            查询
-          </Button>
         </div>
-        <div>
-          <h2>结果：</h2>
-          <div>{response}</div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
-
 export default IndexPage;
