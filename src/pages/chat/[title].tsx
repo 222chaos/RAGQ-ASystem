dfabbdf05a937632a6f74560264b3a6636b99960
期贤 <qixian.cs@antgroup.com>
2024-04-05 21:22:33 +0800

    优化使用体验

--- src/pages/chat/[title].tsx
+++ src/pages/chat/[title].tsx
@@ -5,7 +5,7 @@ import { useTheme } from 'antd-style';
 import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
 import { imageInfoList } from '../Carousel';
-import { Button, Result } from 'antd';
+import { Badge, Button, Result, Space, Tag } from 'antd';
 
 export default function ChatPage() {
   const theme = useTheme();
@@ -46,8 +46,30 @@ export default function ChatPage() {
           height: '100vh',
           width: '100vw',
         }}
+        styles={{
+          chatInputAction: {
+            justifyContent: 'start',
+            padding: 8,
+          },
+        }}
+        actionsRender={() => (
+          <Button
+            color="volcano"
+            size="small"
+            style={{
+              marginRight: 8,
+            }}
+            onClick={() => router.push('/use')}
+            type="primary"
+          >
+            重新选择科目
+          </Button>
+        )}
+        chatItemRenderConfig={{
+          actionsRender: () => <Tag>重新选择科目</Tag>,
+        }}
         helloMessage={`欢迎使用帮你读，你将要查询的科目是 ${selectedImageInfo.title} ，请输入需要查询的内容。`}
-        request={async (messages) => {
+        sendMessageRequest={async (messages) => {
           const requestBody = {
             query: messages,
             selectedImageInfo: selectedImageInfo.title,
